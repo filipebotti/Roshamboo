f6e2e6a5378c413ea6221b3d06916bf0d6ed3fbc
filipebotti <filipebotti@hotmail.com>
2018-10-29 19:32:06 -0300

    finish roshamboo

--- Roshamboo/ResultViewController.swift
+++ Roshamboo/ResultViewController.swift
@@ -10,21 +10,61 @@ import UIKit
 
 class ResultViewController: UIViewController {
 
+    var userValue: String?
+    var computerValue: String?
+    
+    @IBOutlet var roshambooImage: UIImageView!
+    @IBOutlet var roshambooLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        let result = "\(self.userValue!)\(self.computerValue!)"
+        setLabel(result)
+        setImage(result)
+        
+        if self.computerValue != self.userValue {
+            self.roshambooLabel.text = userWon() ? "\(self.roshambooLabel!.text!), you won!" : "\(self.roshambooLabel!.text!), you lose!"
+        }
     }
-    */
-
+    
+    func userWon() -> Bool {
+        print(self.userValue!)
+        print(self.computerValue!)
+        return (self.userValue! == "S" && self.computerValue! == "P") || (self.userValue! == "P" && self.computerValue! == "R") || (self.userValue! == "R" && self.computerValue! == "S")
+    }
+    
+    func setLabel(_ result: String) {
+        switch(result) {
+        case "SP", "PS":
+            self.roshambooLabel.text = "Scissors cuts Paper"
+        case "PR", "RP":
+            self.roshambooLabel.text = "Paper covers Rock"
+        case "RS", "SR":
+            self.roshambooLabel.text = "Rock crushes Scissors"
+        default:
+            self.roshambooLabel.text = "Its a tie!"
+        }
+    }
+    
+    func setImage(_ result: String) {
+        switch(result) {
+        case "SP", "PS":
+            self.roshambooImage.image = UIImage(named: "ScissorsCutPaper")
+        case "PR", "RP":
+            self.roshambooImage.image = UIImage(named: "PaperCoversRock")
+        case "RS", "SR":
+            self.roshambooImage.image = UIImage(named: "RockCrushesScissors")
+        default:
+            self.roshambooImage.image = UIImage(named: "itsATie")
+        }
+    }
+    
+    @IBAction func tryAgain(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
 }
--- Roshamboo/ViewController.swift
+++ Roshamboo/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let options = ["R", "S", "P"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,12 +21,33 @@ class ViewController: UIViewController {
         var controller: ResultViewController
         
         controller = storyboard?.instantiateViewController(withIdentifier: "ResultViewController") as! ResultViewController
+        controller.computerValue = "P"
+        controller.userValue = randomValue()
         
         self.present(controller, animated: true, completion: nil)
     }
     
     @IBAction func scissorsTouched() {
         self.performSegue(withIdentifier: "scissorSegue", sender: self)
+    }
+    
+    func randomValue() -> String {
+        let random = Int.random(in: 0..<3)
+        
+        return options[random]
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        let controller = segue.destination as! ResultViewController
+        
+        controller.computerValue = randomValue()
+        
+        if segue.identifier != nil {
+            controller.userValue = "S"
+        } else {
+            controller.userValue = "R"
+        }
     }
 
 
